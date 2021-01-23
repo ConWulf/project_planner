4fbc28fc3cb56f007403f2fd9c96c782e505993f
Connor Wulf <connor.m.wulf@gmail.com>
2021-01-23 12:10:01 -0600

    adds project filtering

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="filter-nav">
+    <button @click="updateFilter('all')"
+            :class="{active: currentFilter === 'all'}">VIEW ALL</button>
+    <span>|</span>
+    <button @click="updateFilter('complete')"
+            :class="{active: currentFilter === 'complete'}">COMPLETED</button>
+    <span>|</span>
+    <button @click="updateFilter('incomplete')"
+            :class="{active: currentFilter === 'incomplete'}">INCOMPLETE</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "FilterNav",
+  props: ["currentFilter"],
+  methods: {
+    updateFilter (filter) {
+      this.$emit('filterChange', filter);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
